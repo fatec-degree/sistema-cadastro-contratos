ff516b1adb9ff03948999914127aea07d81be998
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-03-16 19:16:27 -0300

    feat: projeto inicial

---
+++ src/test/java/com/fatec/contracts/ContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
