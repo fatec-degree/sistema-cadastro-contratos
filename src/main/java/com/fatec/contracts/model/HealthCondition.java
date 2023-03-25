04792e278cb99e0928461d08668d05dcf08a13a6
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-03-25 15:01:41 -0300

    fix: corrigindo ponto no lugar incorreto

--- src/main/java/com/fatec/contracts/model/HealthCondition.java
+++ src/main/java/com/fatec/contracts/model/HealthCondition.java
@@ -24,6 +24,6 @@ public class HealthCondition {
         return "Doencas: " + sickness + ".\n" +
                "Alergias: " + allergies + ".\n" +
                "Remedios que faz uso: " + medicines + ".\n" +
-               "Observacoes: " + remarks + "\n.";
+               "Observacoes: " + remarks + ".\n";
     }
 }
