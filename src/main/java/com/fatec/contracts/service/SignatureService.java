bb80106f169da383ad821eebb649f346b758532f
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-03-31 22:44:28 -0300

    feat: adicionando métodos para realizar upload de documentos e adicionar signatários

--- src/main/java/com/fatec/contracts/service/SignatureService.java
+++ src/main/java/com/fatec/contracts/service/SignatureService.java
@@ -1,31 +1,51 @@
 package com.fatec.contracts.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@AllArgsConstructor
 @Service
 public class SignatureService {
 
+    @Value("${d4sign.api.token}")
+    private final String TOKEN_API;
+    @Value("${d4sign.api.baseUrl}")
+    private final String BASE_URL;
+
     public void createSignatureList(String uuidDocument) {
+        // Em fase de implementação
+        String signerOne = new SignerBuilder()
+                .email("email")
+                .act(1)
+                .foreign(1)
+                .certificadoIcpBr(0)
+                .assinaturaPresencial(0)
+                .whatsappNumber("11912345678")
+                .build();
+        String signerTwo = new SignerBuilder()
+                .email("email")
+                .act(1)
+                .foreign(1)
+                .certificadoIcpBr(0)
+                .assinaturaPresencial(0)
+                .whatsappNumber("11912345678")
+                .build();
         HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(
-            "{\"signers\":[{" +
-                    "\"email\":\"string\"," +
-                    "\"act\":\"string\"," +
-                    "\"foreign\":\"string\"," +
-                    "\"certificadoicpbr\":\"string\"," +
-                    "\"assinatura_presencial\":\"string\"," +
-                    "\"whatsapp_number\":\"string\"" +
-                "}]}");
+            "{\"signers\":[" + signerOne + ", " + signerTwo + "]}");
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://sandbox.d4sign.com.br/api/v1/documents/" + uuidDocument + "/createlist"))
+                .uri(URI.create(BASE_URL + "/documents/" + uuidDocument + "/createlist"))
                 .header("accept", "application/json")
                 .header("content-type", "application/json")
-                .method("POST", body)
+                .POST(body)
                 .build();
         HttpResponse<String> response;
         try {
@@ -36,20 +56,20 @@ public class SignatureService {
         }
     }
 
-    public void uploadDocument(String uuidSafe) {
-        HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString("\"file\":\"string\",\"uuid_folder\":\"string\"}");
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://sandbox.d4sign.com.br/api/v1/documents/" + uuidSafe + "/upload"))
-                .header("accept", "application/json")
-                .header("content-type", "application/json")
-                .method("POST", body)
-                .build();
-        HttpResponse<String> response;
-        try {
-            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-        } catch (IOException | InterruptedException ex) {
-            throw new RuntimeException("Não foi possível realizar o upload do documento para solicitar as assinaturas.");
+    public HttpResponse<String> uploadDocument(String uuidSafe, String pathToFile) {
+        try(InputStream stream = new FileInputStream(pathToFile)) {
+            HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofInputStream(() -> stream);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(BASE_URL + "/documents/" + uuidSafe + "/upload?tokenApi="+ TOKEN_API))
+                    .headers("Content-Type", "multipart/form-data;",
+                            "accept", "application/json")
+                    .POST(body)
+                    .build();
+            return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException("Não foi possível carregar o arquivo " + pathToFile + ": " + e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Não foi possível realizar o upload do documento: " + e);
         }
     }
 
